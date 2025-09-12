3c59e8892e4513a633769d864ccf9a91e8b510ae
Humberto Junior <astronomi@gmail.com>
2025-09-12 09:57:12 -0300

    Created functions getContract, addTweet and changeUsername

--- dapp/src/services/Web3Service.js
+++ dapp/src/services/Web3Service.js
@@ -1,5 +1,9 @@
 import Web3 from "web3";
 
+import ABI from "./ABI.json";
+
+const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+
 export async function doLogin() {
   if (!window.ethereum) throw new Error("Metamask is not installed");
 
@@ -11,4 +15,27 @@ export async function doLogin() {
   localStorage.setItem("walletAddress", accounts[0]);
 
   return accounts[0];
+}
+
+function getContract() {
+  // if Metamask installed
+  if (!window.ethereum) throw new Error("Metamask is not installed");
+  // start web3 library
+  const web3 = new Web3(window.ethereum);
+  // get wallet in localStorage
+  const from = localStorage.getItem("walletAddress");
+
+  return new web3.eth.Contract(ABI, CONTRACT_ADDRESS, { from });
+}
+
+export async function addTweet() {
+  const contract = getContract();
+
+  await contract.methods.addTweet(text).send();
+}
+
+export async function changeUsername(newName) {
+  const contract = getContract();
+
+  await contract.methods.addTweet(newName).send();
 }
